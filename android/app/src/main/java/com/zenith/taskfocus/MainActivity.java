b71fe5ab35ec7d79b31a4f392d19e7f73b33d3d2
JerryMouseZ <1711333@mail.nankai.edu.cn>
2025-06-22 13:31:00 +0800

    use capacitor as android app

---
+++ android/app/src/main/java/com/zenith/taskfocus/MainActivity.java
@@ -0,0 +1,5 @@
+package com.zenith.taskfocus;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zenith.taskfocus',
+  appName: 'zenith-task-focus',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
